c0c61aaff2b74098c1baf94735e5ebe399a433f1
Rivan20 <rivanmuhammad220203@gmail.com>
2024-05-18 17:11:09 +0700

    Add field in KoTA

--- database/migrations/2024_05_10_095007_create_tbl_kota.php
+++ database/migrations/2024_05_10_095007_create_tbl_kota.php
@@ -14,8 +14,15 @@ return new class extends Migration
         Schema::create('tbl_kota', function (Blueprint $table) {
             $table->integer('id_kota')->unique();
             $table->string('judul');
-            $table->string('nama_mahasiswa');
+            $table->string('nim_satu');
+            $table->string('nama_mahasiswa_satu');
+            $table->string('nim_dua');
+            $table->string('nama_mahasiswa_dua');
+            $table->string('nim_tiga');
+            $table->string('nama_mahasiswa_tiga');
+            $table->string('nip_satu');
             $table->string('pembimbing_satu');
+            $table->string('nim_dua');
             $table->string('pembimbing_dua');
             $table->string('kelas');
             $table->string('periode');
